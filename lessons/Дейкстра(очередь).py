59d5bb147208762a917d774c50e1721521ac5f89
Elena Efimova <elenaefimova@MacBook-Air-Elena.local>
2022-01-19 17:48:29 +0300

    .

--- lessons/Дейкстра(очередь).py
+++ lessons/Дейкстра(очередь).py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def main():
     G = read_graph()
     start = input('С какой вершины начать')
@@ -6,7 +9,42 @@ def main():
 
     shortest_path = reveal_shortest_path(start, finish, shortest_distances)
 
+def read_graph():
+    # М - количество ребер, далее А-Б-вес
+    M = int(input())
+    G = {}
+    for i in range(M):
+        a, b, weight = input().split()
+        weight = float(weight)
+        add_edge(G, a, b, weight)
+        add_edge(G, b, a, weight)
 
+    return G
 
+def add_edge(G, a, b, weight):
+    if a not in G:
+        G[a] = {b: weight}
+    else:
+        G[a][b] = weight
+
+def dijkstra(G, start):
+    Q = deque()
+    # shortest_distances
+    s = {}
+    s[start] = 0
+    Q.push(start)
+    while Q:
+        v = Q.pop()
+        # смежные вершины к v и обновим кратчайщее расстояние у них
+        for u in G[v]:
+            # s[v]+G[v][u] потенциальное кратч расстояние
+            if u not in s or (s[v] + G[v][u]) < s[u]:
+                s[u] = s[v] + G[v][u]
+                Q.push(u)
+    return s
+
+def reveal_shortest_path(start, finish, s):
+    pass
+ 
 if __name__= "__main__":
     main()
